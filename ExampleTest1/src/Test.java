5ec8cbc34c472d31726896f096de20ac73cb6372
Baharan Bahar <40430131+soltani56@users.noreply.github.com>
2020-04-26 14:19:31 +0200

    Fertig Stellung der Test-Klasse

---
+++ ExampleTest1/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String durst = "yes";
+		if(durst == "yes") {
+			System.out.println("Nehmen Sie Bitte etwas zum trinken!");
+		}else {
+			System.out.println("arbeiten Sie Bitte weiter!");
+		}
+		
+		
+		
+
+	}
+
+}
